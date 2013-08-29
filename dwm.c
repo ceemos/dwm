09167fc82f4a6909d8e7b1d296e44908a34f5487
Marcel Schneider <schneiml@studi.informatik.uni-stuttgart.de>
2013-08-29 23:44:33 +0200

    Apply xft patch

--- dwm.c
+++ dwm.c
@@ -54,7 +54,7 @@
 #define WIDTH(X)                ((X)->w + 2 * (X)->bw)
 #define HEIGHT(X)               ((X)->h + 2 * (X)->bw)
 #define TAGMASK                 ((1 << LENGTH(tags)) - 1)
-#define TEXTW(X)                (drw_font_getexts_width(drw->font, X, strlen(X)) + drw->font->h)
+#define TEXTW(X)                (drw_font_getexts_width(drw, X, strlen(X)) + drw->font->h)
 
 #define SYSTEM_TRAY_REQUEST_DOCK    0
 #define _NET_SYSTEM_TRAY_ORIENTATION_HORZ 0
